6cdc5c66e7988198175a73ef8d6315fccb2533f7
Daniel Matilla <d.suarez@sofcloudit.com>
2017-01-18 16:13:06 +0100

    First steps

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -22,17 +22,32 @@ export class HomePage {
   rpmrueda: any;
   aceleracion: any;
 
-  constructor(public navCtrl: NavController, public viewCtrl: ViewController) {}
+  constructor(public navCtrl: NavController, public viewCtrl: ViewController) {
+    this.rpm = 100;
+    this.par = 50;
+    this.pinon = 7;
+    this.corona = 22;
+    this.rueda = 14;
+    this.peso = 40;
+  }
 
   calc() {
     var pi = 3.141592;
-    this.transmision = Math.round(this.corona / this.pinon);
+    this.transmision = this.corona / this.pinon;
     var rpmcorona = this.rpm / this.transmision;
-    this.velocidad = Math.round((rpmcorona / 60) * (this.rueda * pi / 1000));
-    this.vatios = Math.round((this.rpm * this.par) / 400000);
-    var rpms_potencia = this.rpm / 2;
-    this.rpmrueda = Math.round((this.rpm / 2) / this.transmision);
-    var newtons = (30 * this.vatios) / rpms_potencia * (this.rueda / 2000) * pi;
-    this.aceleracion = Math.round((newtons) / (this.peso / 1000) * 100);
+    this.velocidad = (rpmcorona / 60) * (this.rueda * pi / 1000);
+    this.vatios = (this.rpm * this.par) / 400000;
+    this.rpmrueda = (this.rpm / 2) / this.transmision;
+    var newtons = (30 * this.vatios) / this.rpmrueda * (this.rueda / 2000) * pi;
+    this.aceleracion = (newtons) / (this.peso / 1000) * 100;
+    this.round();
+  }
+
+  round() {
+    this.transmision = Math.round(this.transmision * 100) / 100;
+    this.velocidad = Math.round(this.velocidad * 100) / 100;
+    this.vatios = Math.round(this.vatios * 100) / 100;
+    this.rpmrueda = Math.round(this.rpmrueda * 100) / 100;
+    this.aceleracion = Math.round(this.aceleracion * 100) / 100;
   }
 }
